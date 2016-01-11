fdaa4b60136c50a342a4b48ae13e9f4fedce30df
Lucas Pang <lucas.pang@gmail.com>
2016-01-11 13:28:50 -0800

    add some helper methods for parsing email contents out

---
+++ Mailinator.NET/Attachment.cs
@@ -0,0 +1,14 @@
+﻿namespace Mailinator
+{
+    public class Attachment
+    {
+        public string Filename { get; set; }
+        public string FileContentsAsBase64String { get; set; }
+
+        public Attachment(string file, string contents)
+        {
+            Filename = file;
+            FileContentsAsBase64String = contents;
+        }
+    }
+}
--- Mailinator.NET/Email.cs
+++ Mailinator.NET/Email.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace Mailinator
@@ -22,5 +23,51 @@ namespace Mailinator
         public long Time { get; set; }
         [JsonProperty("seconds_ago")]
         public long SecondsAgo { get; set; }
+
+        public string HtmlBody
+        {
+            get
+            {
+                var htmlPart = Parts.FirstOrDefault(p => p.Headers["content-type"].Contains("text/html"));
+                return htmlPart != null ? htmlPart.Body : null;
+            }
+        }
+
+        public string TextBody
+        {
+            get
+            {
+                var textBody = Parts.FirstOrDefault(p => p.Headers["content-type"].Contains("text/plain"));
+                return textBody != null ? textBody.Body : null;
+            }
+        }
+
+        /// <summary>
+        /// Collection of string pairs. Key is the attachment filename and value is Base64 strings representation of attached file
+        /// </summary>
+        public IEnumerable<Attachment> Attachments
+        {
+            get
+            {
+                var col = new List<Attachment>();
+                
+                var attachments = Parts.Where(p => p.Headers["content-type"].Contains("octet-stream"));
+
+                foreach (var attachment in attachments)
+                {
+                    var contentTypes = attachment.Headers["content-type"].Split(';');
+                    var type = contentTypes.First(t => t.Trim().StartsWith("name"));
+                    
+                    var originalFilename = type.Split('=')[1];
+                    var filename = originalFilename.Substring(1, originalFilename.Length - 2); // drop first and last double quote
+
+                    var fileContentsAsString = attachment.Body.Replace("\r\n", string.Empty); // cleanup json body by removing carriage returns
+
+                    col.Add(new Attachment(filename, fileContentsAsString));
+                }
+
+                return col;
+            }
+        }
     }
 }
